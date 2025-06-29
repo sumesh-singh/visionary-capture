4c619cd841cb5ae8fe164504e17ff4be9e65d336
sumeshmainbackup2 inps <inpsrecordedbackup@gmail.com>
2025-06-29 08:29:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/visionary-capture-client.tsx
+++ src/components/visionary-capture-client.tsx
@@ -5,6 +5,7 @@ import { toPng } from 'html-to-image';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-jsx';
